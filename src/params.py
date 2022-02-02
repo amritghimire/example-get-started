2fd09f0009c3d6dcb35f071035d0befa5e900c65
Amrit Ghimire <iamritghimire@gmail.com>
2022-02-02 04:13:37 +0000

    Update src/params.py:DICT.a, src/params.py:DI...
    ...CT.b, src/params.py:FLOAT, src/params.py:INT, src/params.py:LIST, src/params.py:NONE, src/params.py:SET, src/params.py:STR, src/params.py:TUPLE, src/params.py:TestConfig.METRICS, src/params.py:TestConfig.TEST_DIR, src/params.py:TrainConfig.EPOCHS, src/params.py:TrainConfig.layers, src/params.py:UNARY_OP, src/params.py:WEIGHTS.tfidf_jigsaw_regression, src/params.py:WEIGHTS.tfidf_ruddit

--- src/params.py
+++ src/params.py
@@ -1,14 +1,14 @@
 # All standard variable types are supported.
 BOOL = False
-INT = 6
-FLOAT = 0.002
+INT = 4
+FLOAT = 0.003
 STR = "abc"
 DICT = {"a": 1, "b": 2}
-LIST = [1, 2, 3, 4]
-SET = {4, 5, 6, 7}
-TUPLE = (10, 101)
-NONE = None
-UNARY_OP = -2
+LIST = [1, 2, 3, 4, 5]
+SET = {4, 5, 6, 7, 8}
+TUPLE = (10, 102)
+NONE = 1
+UNARY_OP = -3
 
 # Complex expressions will be ignored.
 DICT_EXP = dict(a=1, b=2)
@@ -19,11 +19,11 @@ WEIGHTS = {"tfidf_jigsaw_regression": 3, "tfidf_ruddit": 2}
 
 class TrainConfig:
 
-    EPOCHS = 72
+    EPOCHS = 723
 
     def __init__(self):
         self.layers = 5
-        self.layers = 10  # TrainConfig.layers param will be 9
+        self.layers = 11  # TrainConfig.layers param will be 9
         self.sum = 1 + 2  # Will NOT be found due to the expression
         bar = 3  # Will NOT be found since it's locally scoped
 
